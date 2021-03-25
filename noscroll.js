7d6cb5babeba44615cee909de5ddd335ff4ebd61
Jeff Handley <jeff.handley@microsoft.com>
2021-03-24 19:57:18 -0700

    Remove console.logs

--- noscroll.js
+++ noscroll.js
@@ -1,12 +1,8 @@
 function onWindowBlur() {
     var popupButtons = [...document.querySelectorAll("div[role=button][aria-haspopup=true")];
 
-    console.log('popupButtons', popupButtons && popupButtons.length);
-
     if (popupButtons && popupButtons.length > 2) {
         var emojiButton = popupButtons[2];
-
-        console.log('emojiButton', emojiButton);
 
         if (emojiButton && typeof emojiButton.getClientRects === 'function' && typeof emojiButton.scrollIntoView === 'function') {
             var emojiButtonTop = emojiButton.getClientRects()[0].top;
