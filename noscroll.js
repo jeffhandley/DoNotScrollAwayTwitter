ece76b729c04f92010175d4ab6df78ddeb572d1c
Jeff Handley <jeff.handley@microsoft.com>
2021-03-24 18:46:26 -0700

    Scroll a bit further to prevent Twitter from scrolling

--- noscroll.js
+++ noscroll.js
@@ -1,14 +1,19 @@
 function onWindowBlur() {
     var popupButtons = [...document.querySelectorAll("div[role=button][aria-haspopup=true")];
 
+    console.log('popupButtons', popupButtons && popupButtons.length);
+
     if (popupButtons && popupButtons.length > 2) {
         var emojiButton = popupButtons[2];
+
+        console.log('emojiButton', emojiButton);
 
         if (emojiButton && typeof emojiButton.getClientRects === 'function' && typeof emojiButton.scrollIntoView === 'function') {
             var emojiButtonTop = emojiButton.getClientRects()[0].top;
 
             if (window.scrollY < emojiButtonTop) {
                 emojiButton.scrollIntoView();
+                window.scrollBy(0, 8);
             }
         }
     }
