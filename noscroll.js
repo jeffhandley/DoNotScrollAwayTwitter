08cee41459da68f1bdd5791203ae74c91b231f3b
Jeff Handley <jeff.handley@microsoft.com>
2021-03-24 18:35:43 -0700

    Instead of using a fixed scroll position, use the emoji button position

--- noscroll.js
+++ noscroll.js
@@ -1,6 +1,16 @@
 function onWindowBlur() {
-    if (window.scrollY < 140) {
-        window.scrollTo(window.scrollX, 140);
+    var popupButtons = [...document.querySelectorAll("div[role=button][aria-haspopup=true")];
+
+    if (popupButtons && popupButtons.length > 2) {
+        var emojiButton = popupButtons[2];
+
+        if (emojiButton && typeof emojiButton.getClientRects === 'function' && typeof emojiButton.scrollIntoView === 'function') {
+            var emojiButtonTop = emojiButton.getClientRects()[0].top;
+
+            if (window.scrollY < emojiButtonTop) {
+                emojiButton.scrollIntoView();
+            }
+        }
     }
 }
 
